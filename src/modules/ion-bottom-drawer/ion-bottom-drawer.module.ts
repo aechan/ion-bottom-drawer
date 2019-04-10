88a9e483415da0261770314bdecef8b2280c1f88
Alec Chan <me@alecchan.io>
2019-04-09 19:38:47 -0500

    Update ion-bottom-drawer.module.ts

--- src/modules/ion-bottom-drawer/ion-bottom-drawer.module.ts
+++ src/modules/ion-bottom-drawer/ion-bottom-drawer.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { IonicModule } from 'ionic-angular';
+import { IonicModule } from '@ionic/angular';
 import { IonBottomDrawerComponent } from './ion-bottom-drawer';
 
 @NgModule({
